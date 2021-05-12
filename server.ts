112ec3bdfe18a5a0f2904bfb9aabba87147dbc94
Anthony Maladot <anthony.maladot@netc.fr>
2021-05-12 21:13:36 +0200

    remove host listening

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 const server: Server = new Server(app);
 const port: number = parseInt(process.env.port, 10) || 5000;
 
-app.listen(port, 'localhost', function(err: any) {
+app.listen(port, function (err: any) {
   if (err) return err;
   console.info(`Server running on : http://localhost:${port}`);
 });
