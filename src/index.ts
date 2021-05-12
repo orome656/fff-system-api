4f4784989988e5619648e9893af20ce7bad39286
Anthony Maladot <anthony.maladot@netc.fr>
2021-05-12 23:28:42 +0200

    Add basic auth

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,26 @@ export default class Server {
       path.join(__dirname, "./logs/access.log"),
       { flags: "a" }
     );
+    app.use((req, res, next) => {
+
+      const auth = {
+        login: process.env.api_user, password: process.env.api_password
+      }
+
+      const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+      const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+      if (login && password && login === auth.login && password === auth.password) {
+        return next()
+      }
+
+      res.set('WWW-Authenticate', 'Basic realm="401"')
+      res.status(401).send('Authentication required.')
+
+      // -----------------------------------------------------------------------
+
+    })
+
     app.use(morgan("combined", { stream: accessLogStream }));
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
